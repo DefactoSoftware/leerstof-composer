e9418b2135472510eae9d6b882c549e3854a5981
Frits Ypeij <frits@defacto.nl>
2020-12-09 13:07:46 +0100

    Add jquery, make editing lists easier

--- js/script.js
+++ js/script.js
@@ -1,33 +1,73 @@
-save = function () {
-  html2canvas(document.querySelector("#container")).then(canvas => {
-    canvas.toBlob(function (blob) {
-      let filename = prompt('Enter file name');
-      saveAs(blob, filename + ".png");
+$(function () {
+  save = function () {
+    html2canvas(document.querySelector("#container")).then(canvas => {
+      canvas.toBlob(function (blob) {
+        let filename = prompt('Enter file name');
+
+        if (!filename) {
+          filename = "untitled";
+        }
+
+        saveAs(blob, filename + ".png");
+      });
     });
-  });
-};
+  };
 
-function formatDoc(cmd, val) {
-  document.execCommand(cmd, false, val);
-}
-
-add_item = function (focussed_li) {
-  const li = document.createElement("li");
-  li.setAttribute("contenteditable", "true");
-  li.setAttribute("class", "li");
-  insertAfter(li, focussed_li);
-  li.focus();
-};
-
-insertAfter = function (newNode, referenceNode) {
-  referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
-}
-
-document.querySelector(".list").addEventListener("keydown", function (e) {
-  var focussed_li = document.activeElement;
-  if (e.keyCode === 13) {
+  add_item = function (focused_li, e) {
     e.preventDefault();
     e.stopPropagation();
-    add_item(focussed_li);
-  }
+
+    $("<li>").addClass("li").prop("contenteditable", true).insertAfter(focused_li).focus();
+  };
+
+  remove_item = function (focused_li, e) {
+    e.stopPropagation();
+    focused_li.find("br:last-child").remove();
+
+    if (focused_li.is(':empty') && $("li").length > 1) {
+      if (prev_item(focused_li, e) != false) {
+        focused_li.remove();
+      } else if (!next_item(focused_li, e) != false) {
+        focused_li.remove();
+      }
+    }
+  };
+
+  prev_item = function (focused_li, e) {
+    e.preventDefault();
+    if (focused_li.prev().length) {
+      focused_li.prev().focus();
+    } else {
+      return false;
+    }
+  };
+
+  next_item = function (focused_li, e) {
+    e.preventDefault();
+    if (focused_li.next().length) {
+      focused_li.next().focus();
+    } else {
+      return false;
+    }
+  };
+
+  $(".list").on("keydown", function (e) {
+    var focused_li = document.activeElement;
+
+    if (e.keyCode === 13) { // return
+      add_item($(focused_li), e);
+    }
+
+    if (e.keyCode === 38) { // up
+      prev_item($(focused_li), e);
+    }
+
+    if (e.keyCode === 40) { // down
+      next_item($(focused_li), e);
+    }
+
+    if (e.keyCode === 8) { // backspace
+      remove_item($(focused_li), e);
+    }
+  });
 });
